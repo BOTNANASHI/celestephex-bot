8f8aac261b9e8e308d97a114e397cc49e5046a91
BOTNANASHI <78802603+BOTNANASHI@users.noreply.github.com>
2025-01-16 23:11:40 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,13 @@
-const { default: makeWASocket } = require('@adiwajshing/baileys');
+const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require('@adiwajshing/baileys');
 const { log } = require('./src/utils/logger');
 const { pingCommand } = require('./src/commands/ping');
 
 async function startBot() {
-    const sock = makeWASocket();
+    const sock = makeWASocket({
+        printQRInTerminal: true
+    });
 
+    // Monitora mensagens de grupo
     sock.ev.on('messages.upsert', async (msg) => {
         const message = msg.messages[0];
         if (!message.message) return;
@@ -16,7 +19,30 @@ async function startBot() {
             await pingCommand(sock, sender);
         }
 
-        log(`Mensagem de ${sender}: ${text}`);
+        log(`Mensagem de ${sender}: ${vtnc viado kkkkkk}`);
+    });
+
+    // Monitora eventos de entrada de novos membros
+    sock.ev.on('group-participants.update', async (update) => {
+        const { participants, jid } = update;
+        if (update.action === 'add') {
+            const newMember = participants[0];
+            // Envia mensagem de boas-vindas para o novo membro
+            await sock.sendMessage(jid, { text: `🎉 seja bem vindo(a) a CΞLΞSTPHEX e NARNIA TEAM, Nossa guilda tem o maior prazer em receber você.
+Entrem na nossa guilda : 2055173781
+São no mínimo 30 dias pra colocar a tag : "ᶜᣖˣ " ja com espaço invisível. daremos no Mínimo 30 dias pra colocar a tag, mt obrigado qualquer dúvida chame os adms no pv🫡, @${newMember}! 🎉`, mentions: [newMember] });
+            log(`Novo membro adicionado: @${newMember}`);
+        }
+    });
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            const reason = lastDisconnect?.error?.output?.statusCode;
+            if (reason === DisconnectReason.loggedOut) {
+                log('Desconectado. Faça login novamente.');
+            }
+        }
     });
 
     log("Bot iniciado com sucesso!");
